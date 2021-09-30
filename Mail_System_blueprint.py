d7fee598d7a6bfeed3fb29d566af0f0733a4a764
lordhj <harshitjoshi450@gmail.com>
2021-09-30 19:30:12 +0530

    Raw template for msg body

--- Mail_System_blueprint.py
+++ Mail_System_blueprint.py
@@ -1,7 +1,7 @@
 from tkinter import *
 window = Tk()
 window.title("MAIL System")
-window.config(width=640, height=480)
+window.config(width=640, height=480, padx=20, pady=20)
 
 #--------UI Elements--------#
 
@@ -19,5 +19,13 @@ to_mail_label_entry.grid(row=2, column=1)
 #Buttons
 login_button = Button(text="Login")
 login_button.grid(row=3, column=0)
+
+message_entry = Text(height=5, width=40)
+message_entry.focus()
+message_entry.insert(END, "Type your message here")
+message_entry.grid(row=4, column=0, columnspan=2)
+
+send_button = Button(text="Send")
+send_button.grid(row=5,column=0)
 
 window.mainloop()
